1adccab12675b08a3b787e38e1d884158a209f96
miox <gallantsummerwind@gmail.com>
2023-10-24 01:08:04 +0900

    Menuのコンポーネントの修正

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,24 +3,25 @@ import React from 'react'
 export const Menu = () => {
     return (
         <>
-            {/* <nav class="header-secnav">
-                    <ul class="header-navlist">
-                        <li class="header-navitem">
-                            <a href="#">予告</a>
-                        </li>
-                        <li class="header-navitem">
-                            <a href="#">内容</a>
-                        </li>
-                        <li class="header-navitem">
-                            <a href="#">プレゼント</a>
-                        </li>
-                        <li class="header-navitem">
-                            <a href="#">SNS</a>
-                        </li>
-                        <li class="header-navitem">
-                            <a href="#">意見</a>
-                        </li>
-                </nav> */}
+            <nav class="flex justify-around text-[#fff] bg-[#092080]">
+                <ul class="flex justify-around">
+                    <li class="">
+                        <a class="text-2xl px-5 py-2" href="#">予告</a>
+                    </li>
+                    <li class="border-l-2 border-s-indigo-500">
+                        <a class="text-2xl px-5 py-2" href="#">内容</a>
+                    </li>
+                    <li class="border-x-2 border-s-indigo-500">
+                        <a class="text-2xl px-5 py-2" href="#">プレゼント</a>
+                    </li>
+                    <li class="border-r-2 border-s-indigo-500">
+                        <a class="text-2xl px-5 py-2" href="#">SNS</a>
+                    </li>
+                    <li class="">
+                        <a class="text-2xl px-5 py-2" href="#">意見</a>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
