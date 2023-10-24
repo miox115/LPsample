ed4785550ddba9fe61b753d962f5eb6e380a05b2
miox <gallantsummerwind@gmail.com>
2023-10-24 11:29:39 +0900

    HederのIcon追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
+import { FaCalendarAlt } from 'react-icons/fa';
+import { GiOctopus } from 'react-icons/gi';
 
 export const Header = () => {
     return (
         <>
             <header class="flex justify-around">
-                <a class="m-2" href="#">img</a>
+                <h3 class="m-2 pt-1"> <GiOctopus size={40} color={'#ccc'} /></h3>
                 <ul class="flex justify-around">
-                    <li class="m-2">
+                    <li class="m-2 pt-3">
                         <a href="#">ラジオ</a>
                     </li>
-                    <li class="m-2">
+                    <li class="m-2  pt-3">
                         <a href="#">ドラマ</a>
                     </li>
-                    <li class="m-2">
+                    <li class="m-2  pt-3">
                         <a href="#">アニメ</a>
                     </li>
-                    <li class="m-2">
+                    <li class="m-2  pt-3">
                         <a href="#">スポーツ</a>
                     </li>
-                    <li class="m-2">
+                    <li class="m-2  pt-3">
                         <a href="#">エンタメ</a>
                     </li>
-                    <li class="m-2">
+                    <li class="m-2  pt-3">
                         <a href="#">音楽</a>
                     </li>
                 </ul>
-                <a class="m-2" href="#">img</a>
+                <h3 class="m-2 pt-1"> < FaCalendarAlt size={35} color={'#ccc'}/></h3>
             </header>
 
 
